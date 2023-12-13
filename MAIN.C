1bfa903ec6a7c4a7dee283352178c713d5cb40e4
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2023-12-13 19:28:35 +0300

    Main.c

--- MAIN.C
+++ MAIN.C
@@ -1,32 +1,44 @@
 #include "shell.h"
 
 /**
- * main - Entry point for the shell program
- * @argc: Number of command-line arguments
- * @argv: Array of command-line argument strings
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-    info_t info = INFO_INIT;
-    int fd;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-    if (argc == 2)
-    {
-        fd = open(argv[1], O_RDONLY);
-        if (fd == -1)
-        {
-            perror(argv[0]);
-            exit(EXIT_FAILURE);
-        }
-        info.readfd = fd;
-    }
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-    populate_env_list(&info);
-    read_history(&info);
-    hsh(&info, argv);
-
-    return EXIT_SUCCESS;
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
-
